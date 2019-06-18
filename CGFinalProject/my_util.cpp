3c70148bab3522c7a3657293368c011d449fdf4e
rhino <1254086477@qq.com>
2019-06-18 23:47:03 +0800

    完成skyBox

--- CGFinalProject/my_util.cpp
+++ CGFinalProject/my_util.cpp
@@ -4,3 +4,6 @@ float distanceOfPositions(const glm::vec3 pos1, const glm::vec3 pos2) {
 	glm::vec3 pos = pos1 - pos2;
 	return (float)sqrt(pos.x * pos.x + pos.y * pos.y + pos.z * pos.z);
 }
+
+const unsigned int SCR_WIDTH = 800 * 2;
+const unsigned int SCR_HEIGHT = 600 * 2;
---
+++ CGFinalProject/skyBox.h
@@ -0,0 +1,96 @@
+#ifndef SKY_BOX__H
+#define SKY_BOX__H
+
+#include "AnimatedModel.h"
+#include <learnopengl/camera.h>
+#include <learnopengl/shader.h>
+#include <string>
+#include "stb_image.h"
+#include <iostream>
+#include "ogldev_util.h"
+
+using std::string;
+
+class SkyBox
+{
+public:
+	SkyBox(Camera* camera,
+		const string& PosXFilename = "resources/skyBox/sp3right.jpg",
+		const string& NegXFilename = "resources/skyBox/sp3left.jpg",
+		const string& PosYFilename = "resources/skyBox/sp3top.jpg",
+		const string& NegYFilename = "resources/skyBox/sp3bot.jpg",
+		const string& PosZFilename = "resources/skyBox/sp3front.jpg",
+		const string& NegZFilename = "resources/skyBox/sp3back.jpg"
+	)
+		:skyModel("resources/skyBox/sphere.obj"),
+		skyBoxShader("skyBox.vs", "skyBox.fs"),
+		camera(camera),
+		texutreFileName{ PosXFilename , NegXFilename, PosYFilename, NegYFilename, PosZFilename, NegZFilename }
+	{
+	}
+	~SkyBox() {
+
+	}
+	void init() {
+		const unsigned int types[6] = { GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+									    GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+										GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+										GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+										GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+										GL_TEXTURE_CUBE_MAP_NEGATIVE_Z };
+		glGenTextures(1, &textureID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+		for (unsigned int i = 0; i < ARRAY_SIZE_IN_ELEMENTS(types); i++) {
+			int width, height, nrChannels;
+			unsigned char *data = stbi_load(texutreFileName[i].c_str(), &width, &height, &nrChannels, 0);
+			if (data)
+			{
+				glTexImage2D(types[i], 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			}
+			else
+			{
+				std::cout << "Failed to load texture" << std::endl;
+			}
+
+			stbi_image_free(data);
+		}
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	}
+	void Draw() {
+		skyBoxShader.use();
+		GLint OldCullFaceMode;
+		glGetIntegerv(GL_CULL_FACE_MODE, &OldCullFaceMode);
+		GLint OldDepthFuncMode;
+		glGetIntegerv(GL_DEPTH_FUNC, &OldDepthFuncMode);
+		glCullFace(GL_FRONT);
+		glDepthFunc(GL_LEQUAL);
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, camera->Position);
+		model = glm::scale(model, glm::vec3(20.0f, 20.0f, 20.0f));
+		glm::mat4 projection = glm::perspective(glm::radians(camera->Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 10000.0f);
+		glm::mat4 view = camera->GetViewMatrix();
+		skyBoxShader.setMat4("model", model);
+		skyBoxShader.setMat4("projection", projection);
+		skyBoxShader.setMat4("view", view);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+		skyModel.Draw(skyBoxShader, 0.0f); // 无动作，时间不重要
+
+		glCullFace(OldCullFaceMode);
+		glDepthFunc(OldDepthFuncMode);
+	}
+private:
+	AnimatedModel skyModel;
+	Camera* camera;
+	Shader skyBoxShader;
+	unsigned int textureID;
+	const string texutreFileName[6];
+};
+
+
+#endif // !SKY_BOX__H
